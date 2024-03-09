8a762ce44b03abb0ab6dd7975060e5c903eadf44
oksid <oksid123123@gmail.com>
2024-03-09 14:05:52 +0400

    Add Category service interface

---
+++ UserCollection.Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using UserCollection.WebAPI.Models;
+
+namespace UserCollection.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Task AddCategory(CollectionCategoryModel category);
+
+        public Task UpdateCategory(CollectionCategoryModel category);
+
+        public Task DeleteCategory(CollectionCategoryModel category);
+
+        public Task<CollectionCategoryModel> GetCategory(int id);
+
+        public Task<IEnumerable<CollectionCategoryModel>> GetAllCategories();
+    }
+}
