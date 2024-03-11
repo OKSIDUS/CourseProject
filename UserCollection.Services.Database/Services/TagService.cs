1c2d188b62a9cb1707403f63d83202372d68f74c
oksid <oksid123123@gmail.com>
2024-03-11 17:31:39 +0400

    Add TagService Interface

---
+++ UserCollection.Services.Database/Services/TagService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using UserCollection.Services.Interfaces;
+using UserCollection.WebAPI.Models;
+
+namespace UserCollection.Services.Database.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly ApplicationDBContext dbContext;
+        private readonly IMapper mapper;
+
+        public TagService(ApplicationDBContext dbContext, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        public Task AddTag(TagModel tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTag(TagModel tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TagModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TagModel> GetTagById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTag(TagModel tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UserCollection.Services/Interfaces/ITagService.cs
@@ -0,0 +1,17 @@
+﻿using UserCollection.WebAPI.Models;
+
+namespace UserCollection.Services.Interfaces
+{
+    public interface ITagService
+    {
+        public Task AddTag(TagModel tag);
+
+        public Task UpdateTag(TagModel tag);
+
+        public Task DeleteTag(TagModel tag);
+
+        public Task<TagModel> GetTagById(int id);
+
+        public Task<IEnumerable<TagModel>> GetAll();
+    }
+}
