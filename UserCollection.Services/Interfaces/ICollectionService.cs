d375f4c1f59b5b6974e07bb870de3ad89be9edb2
oksid <oksid123123@gmail.com>
2024-03-11 14:30:25 +0400

    Add CollectionService Interface

---
+++ UserCollection.Services/Interfaces/ICollectionService.cs
@@ -0,0 +1,17 @@
+﻿using UserCollection.WebAPI.Models;
+
+namespace UserCollection.Services.Interfaces
+{
+    public interface ICollectionService
+    {
+        public Task AddCollectionAsync(CollectionModel collection);
+
+        public Task DeleteCollectionAsync(int id);
+
+        public Task<CollectionModel> GetCollectionAsync(int id);
+
+        public Task<IEnumerable<CollectionModel>> GetAllCollectionsAsync();
+
+        public Task UpdateCollection(CollectionModel collection);
+    }
+}
