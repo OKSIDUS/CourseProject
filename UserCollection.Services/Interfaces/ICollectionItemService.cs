f451c9959ac65a87bd5fe0c924d9fcb2c00e1f9f
oksid <oksid123123@gmail.com>
2024-03-11 16:28:56 +0400

    Add ItemService Interface

---
+++ UserCollection.Services/Interfaces/ICollectionItemService.cs
@@ -0,0 +1,19 @@
+﻿using UserCollection.WebAPI.Models;
+
+namespace UserCollection.Services.Interfaces
+{
+    public interface ICollectionItemService
+    {
+        public Task AddItem(ItemModel item);
+
+        public Task UpdateItem(ItemModel item);
+
+        public Task DeleteItem(int id);
+
+        public Task<ItemModel> GetItemById(int id);
+
+        public Task<IEnumerable<ItemModel>> GetAll();
+
+
+    }
+}
